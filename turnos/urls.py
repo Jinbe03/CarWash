1b0a9064105c661d8a4512264a510cd882cd3a24
Jinbe03 <143218234+Jinbe03@users.noreply.github.com>
2025-12-30 12:15:18 -0300

    Update urls.py

--- turnos/urls.py
+++ turnos/urls.py
@@ -4,5 +4,5 @@ from . import views
 urlpatterns = [
     path('', views.home, name='home'),
     path('reservas/', views.reservas, name='reservas'),
-    
+    path('precios/', views.lista_precios, name='lista_precios'),
 ]
